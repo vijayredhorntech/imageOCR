ddef2ab3115b3cf528f33283b827abb509827fd0
vijayredhorntech <vijay@redhorntech.com>
2024-09-11 11:30:29 +0530

    default port

--- main.py
+++ main.py
@@ -28,4 +28,4 @@ async def perform_ocr(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=1000)  # Explicitly bind to port 1000
+    uvicorn.run(app, host="0.0.0.0")
